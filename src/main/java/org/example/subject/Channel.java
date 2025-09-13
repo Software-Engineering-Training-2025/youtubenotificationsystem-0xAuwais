5ea17b697c14c29688c27731e7d8c23f44105c7d
Auwais <auwais.albahra@gmail.com>
2025-09-13 20:01:19 +0300

    Used Observer Pattern

--- src/main/java/org/example/subject/Channel.java
+++ src/main/java/org/example/subject/Channel.java
@@ -2,30 +2,43 @@ package org.example.subject;
 
 import org.example.observer.Observer;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Channel implements Subject{
     private final String name;
-
+    private String messege;
+    List<Observer> observers= Collections.synchronizedList(new ArrayList<Observer>());
     public Channel(String name) {
         this.name = name;
     }
 
     public void uploadVideo(String title){
-        // TODO: Notify all subscribers about new video
+
+        this.messege=title;
     }
 
     @Override
     public void subscribe(Observer observer) {
-        // TODO: Add subscriber
+        observers.add(observer);
     }
 
     @Override
     public void unsubscribe(Observer observer) {
-        // TODO: Remove subscriber
+        if(observers.contains(observer))
+        observers.remove(observer);
+        else{
+            System.out.println("Observer Not Found");
+        }
     }
 
     @Override
     public void notifyObservers(String message) {
-        // TODO: Notify subscribers
+        this.messege=message;
+        for(Observer observer:observers){
+            observer.update(message);
+        }
     }
 
     public String getName() {
